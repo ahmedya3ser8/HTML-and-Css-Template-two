9cf753fc7d76413deedf2ed3a7669f971300770f
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-04-13 08:34:45 +0200

    Creating Some Features And Improve Some Sections with Js

---
+++ main.js
@@ -0,0 +1,63 @@
+let switcherLis = document.querySelectorAll(".shuffle li");
+let box = document.querySelectorAll(".imgs-holder .box");
+
+switcherLis.forEach((li) => {
+  li.addEventListener("click", RemoveActive);
+  li.addEventListener("click", removeBox);
+});
+
+// Remove Active Class from all li and add active class to current li
+
+function RemoveActive() {
+  switcherLis.forEach((li) => {
+    li.classList.remove("active");
+    this.classList.add("active");
+  });
+}
+
+// Manage Imgs
+
+function removeBox() {
+  box.forEach((el) => {
+    el.style.display = "none";
+  });
+  document.querySelectorAll(this.dataset.filter).forEach((el) => {
+    el.style.display = "block";
+  });
+}
+
+// Animated our-skills
+
+let section = document.querySelector(".our-skills");
+let spansProgress = document.querySelectorAll(".prog-holder span");
+
+window.onscroll = function() {
+  if (window.scrollY >= section.offsetTop - 300) {
+    spansProgress.forEach((span) => {
+      span.style.width = span.dataset.progress;
+    });
+  }
+}
+
+let statsSection = document.querySelector(".stats");
+let number = document.querySelectorAll(".stats .numbers");
+let started = false;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= statsSection.offsetTop - 350) {
+    if (!started) {
+      number.forEach((n) => startCount(n));
+    }
+    started = true;
+  }
+});
+
+function startCount(ele) {
+  let numTarget = ele.dataset.num;
+  let counter = setInterval(() => {
+    ele.textContent++;
+    if (ele.textContent == numTarget) {
+      clearInterval(counter);
+    }
+  }, 1000 / numTarget);
+}
